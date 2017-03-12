c7776698b46feeff68f1f0fc149a612af0ba17cf
Augustus P Zhang <musixhine@gmail.com>
2017-03-12 18:37:44 -0400

    Fine Tune + Add No Goody Huts.

---
+++ NoGoodyHuts.sql
@@ -0,0 +1,2 @@
+INSERT INTO ConfigurationUpdates (SourceGroup, SourceId, SourceValue, TargetGroup, TargetId, TargetBalue, Hash)
+VALUES ('Game', 'RULESET', 'RULESET_REALMARATHON', 'Game', 'GAME_NO_GOODY_HUTS', 1, 0)
