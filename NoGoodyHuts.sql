8ce8863608e7e38b533d62811275fa2fe10134ed
Augustus P Zhang <musixhine@gmail.com>
2017-03-12 19:55:30 -0400

    Fine Tune

--- NoGoodyHuts.sql
+++ NoGoodyHuts.sql
@@ -1,2 +1,2 @@
-INSERT INTO ConfigurationUpdates (SourceGroup, SourceId, SourceValue, TargetGroup, TargetId, TargetBalue, Hash)
-VALUES ('Game', 'RULESET', 'RULESET_REALMARATHON', 'Game', 'GAME_NO_GOODY_HUTS', 1, 0)
+INSERT INTO ConfigurationUpdates (SourceGroup, SourceId, SourceValue, TargetGroup, TargetId, TargetValue, Hash)
+VALUES ('Game', 'RULESET', 'RULESET_REALMARATHON', 'Game', 'GAME_NO_GOODY_HUTS', 1, 0);
